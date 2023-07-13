2cac2befd8b56fe97664e1fb23e43e7b195b99b8
Celeste Avalos <celesteavalos93@gmail.com>
2023-07-13 16:20:22 -0300

    cambios en carpetas

--- js/cards.js
+++ js/cards.js
@@ -1,3 +1,5 @@
+const contenedor = document.getElementById('container');
+
 const  miCards = document.getElementsByClassName('cards');
 
 const  cardImg = document.getElementsByClassName('card');
@@ -7,3 +9,15 @@ const  cardInfo = document.getElementsByClassName('card-body');
 const  button = document.getElementsByClassName('btn');
 
 console.log (cardInfo);
+
+let carrito = [];
+let stock = [];
+
+stock.push(new Articulo('Mantel', 1300));
+stock.push(new Articulo('Vasos', 800));
+stock.push(new Articulo('Cuadros', 950));
+stock.push(new Articulo('Ollas', 1580));
+stock.push(new Articulo('Cubiertos', 760));
+stock.push(new Articulo('Lapices', 500));
+
+localStorage.setItem('stock',JSON.stringify(stock));

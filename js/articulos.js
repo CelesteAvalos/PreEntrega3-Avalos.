cdef44b9cb27da02e2ab60445b1cf6cae40a51e1
Celeste Avalos <celesteavalos93@gmail.com>
2023-07-13 00:41:57 -0300

    iniciando proyecto

---
+++ js/articulos.js
@@ -0,0 +1,8 @@
+const articulos = [
+    {nombre: 'Vasos' , seccion: 'Vajilla' , precio: 800},
+    {nombre: 'Mantel' , seccion: 'Textil' , precio: 1300},
+    {nombre: 'Cuadros' , seccion: 'Decoracion' , precio: 950},
+    {nombre: 'Ollas' , seccion: 'Cocina' , precio: 1580},
+    {nombre: 'Cubiertos' , seccion: 'Cocina' , precio: 760},
+    {nombre: 'Lapices' , seccion: 'Libreria' , precio: 500},
+];
---
+++ js/cards.js
@@ -0,0 +1,9 @@
+const  miCards = document.getElementsByClassName('cards');
+
+const  cardImg = document.getElementsByClassName('card');
+
+const  cardInfo = document.getElementsByClassName('card-body');
+
+const  button = document.getElementsByClassName('btn');
+
+console.log (cardInfo);

c80170145e9537e2cc400286b1171c3acb087b0d
Celeste Avalos <celesteavalos93@gmail.com>
2023-07-13 22:46:39 -0300

    nuevos cambios

--- js/cards.js
+++ js/cards.js
@@ -8,12 +8,16 @@ const  cardInfo = document.getElementsByClassName('card-body');
 
 const  button = document.getElementsByClassName('btn');
 
+const tabla = document.getElementById('items');
+
+const total = document.getElementById('total');
+
 console.log (cardInfo);
 
 let carrito = [];
 let stock = [];
 
-stock.push(new Articulo('Mantel', 1300));
+stock.push(new Articulo('mantel', 1300));
 stock.push(new Articulo('Vasos', 800));
 stock.push(new Articulo('Cuadros', 950));
 stock.push(new Articulo('Ollas', 1580));
@@ -21,3 +25,20 @@ stock.push(new Articulo('Cubiertos', 760));
 stock.push(new Articulo('Lapices', 500));
 
 localStorage.setItem('stock',JSON.stringify(stock));
+ 
+function traerItems() {
+
+    stock = JSON.parse(localStorage.getItem('stock')) || [];
+    carrito = JSON.parse(localStorage.getitem('carrito')) || [];
+}
+
+function contenedoresCards()  {
+
+    stock.forEach((articulo,index) =>  {
+
+        let option = document.createElement('option');
+        option.textContent = articulo.nombre;
+        option.value = index;
+        selectArticulos.appendChild(option);
+    })
+}
